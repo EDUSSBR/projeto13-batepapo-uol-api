4dbca13aa70f7ac95397e090a57c25316e9d8676
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-04-12 12:04:49 -0300

    add some db configs

---
+++ src/db/index.js
@@ -0,0 +1,26 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import * as dotenv from 'dotenv';
+dotenv.config();
+let db
+export default db = (async  (host=process.env.DATABASE_URL)=> {
+    let conn;
+    const client = new MongoClient(host, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    })
+    
+    try {
+        console.log("connecting to MongoDB...")
+        conn = await client.connect().then((client)=>{
+            console.log("Mongo is sucessfully connected!")
+            return client
+        })
+
+        return conn
+    } catch (e) {
+        console.log(e)
+    }
+})().then(client => client.db())
