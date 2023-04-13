0bb22e62d9f25115f9234169bee8d397ce480903
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-04-13 12:43:06 -0300

    changes for passing test

--- src/db/index.js
+++ src/db/index.js
@@ -1,23 +1,16 @@
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import { MongoClient } from 'mongodb';
 import * as dotenv from 'dotenv';
 dotenv.config();
 let db
 export default db = (async  (host=process.env.DATABASE_URL)=> {
     let conn;
-    const client = new MongoClient(host, {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        }
-    })
+    const client = new MongoClient(host)
     
     try {
         console.log("connecting to MongoDB...")
-        return await client.connect().then((client)=>{
-            console.log("Mongo is sucessfully connected!")
-            return client
-        })
+        const conn = await  client.connect() 
+        console.log("Mongo is sucessfully connected!")
+        return conn
         
     } catch (e) {
         console.log(e)
