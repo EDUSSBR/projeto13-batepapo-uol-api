3c2140c90a54d8a6f57dfb1afa30f4fe3769f512
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-04-13 12:03:27 -0300

    add get/delete/update message by id to message repo

--- src/repositories/messagesRepository.js
+++ src/repositories/messagesRepository.js
@@ -1,3 +1,5 @@
+import { ObjectId } from 'mongodb'
+
 let db = await import('../db/index.js')
 db = await db.default
 
@@ -6,7 +8,16 @@ export const messagesRepository = {
     getMessages: function getMessages(){
         return this.db.find({}).toArray()
     },
+    getMessageById: function getMessageById(id){
+        return this.db.findOne({_id: new ObjectId(id)})
+    },
+    deleteMessageById: function deleteMessageById(id){
+        return this.db.deleteOne({_id: new ObjectId(id)})
+    },
     createMessage: function createMessage(message) {
         return this.db.insertOne(message)
+    },
+    updateMessage: function updateMessage(id, message) {
+        return this.db.updateOne({_id: new ObjectId(id)}, { "$set": { text: message}})
     },
 }
