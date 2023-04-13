139316d663f3ebbc8c7e4680dda90d07407ee687
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-04-13 11:58:48 -0300

    add id validation to user and participant schema

--- src/entities/schemas/messageSchema.js
+++ src/entities/schemas/messageSchema.js
@@ -1,6 +1,7 @@
 import Joi from "joi";
 
 export const messageSchema =  Joi.object({
+    id: Joi.string().length(24).pattern(/[0-9a-fA-F]+/),
     from: Joi.string().min(3).max(30).required(),
     to: Joi.string().min(3).max(30).required(),
     text: Joi.string().max(500).required(),
--- src/entities/schemas/participantSchema.js
+++ src/entities/schemas/participantSchema.js
@@ -1,5 +1,6 @@
 import Joi from "joi";
 export const participantSchema = Joi.object({
+    id: Joi.string().length(24).pattern(/[0-9a-fA-F]+/),
     name: Joi.string().min(3).max(30).required(),
     lastStatus: Joi.date().timestamp()
 })
