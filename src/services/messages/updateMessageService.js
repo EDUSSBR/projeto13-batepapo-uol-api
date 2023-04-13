17bc374b0580f75bd3b8e1c8adcd81acccb7f977
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-04-13 12:04:22 -0300

    add updateMessageByIdService

---
+++ src/services/messages/updateMessageService.js
@@ -0,0 +1,18 @@
+import { messagesRepository } from "../../repositories/messagesRepository.js"
+import { participantsRepository } from "../../repositories/participantsRepository.js"
+import { createMessage } from "../../entities/message.js"
+
+export async function updateMessageService({id, from, text}) {
+    const messageExists = await messagesRepository.getMessageById(id);
+    const participantExists = await participantsRepository.getParticipantsByName(from)
+
+    // if (participantExists === null) {
+    //     throw { message: "user does not exists", status: 422 }
+    // }
+    if (messageExists === null || participantExists===null){
+        throw { status: 404}
+    } else if (from !==messageExists.from){
+        throw { status: 401}
+    }
+    await messagesRepository.updateMessage(id, {text});
+}
