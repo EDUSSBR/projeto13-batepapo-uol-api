e6c726ecc0c190a267177c714bd6fbd74eeb784d
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-04-13 11:58:11 -0300

    feat: should delete participants from room every 15 seconds if status is not updated in the last 10 seconds

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,10 @@ import cors from "cors";
 import participantsRouter from './participantRouter.js';
 import messagesRouter from './messageRouter.js';
 import status from './statusRouter.js'
+import { getParticipantsService } from "../services/participants/getParticipantsService.js";
+import { createMessage } from "../entities/message.js";
+import { messagesRepository } from "../repositories/messagesRepository.js";
+import { participantsRepository } from "../repositories/participantsRepository.js";
 
 const app = express();
 
@@ -11,5 +15,21 @@ app.use(cors());
 app.use(participantsRouter);
 app.use(messagesRouter);
 app.use(status);
+
+setInterval(async () => {
+    try{
+        const participants = await getParticipantsService()
+        if (participants.length===0 || participants===null) return 
+        for (let i=0; i<participants.length; i++){
+            if (Date.now() - participants[i].lastStatus > 10000) {
+                const message = createMessage(participants[i].name, "sai da sala...")
+                messagesRepository.createMessage(message)
+                participantsRepository.removeParticipant(participants[i]._id)
+            } 
+        }
+    } catch (e){
+        console.log(e)
+    }
+}, 15000)
 
 export default app
