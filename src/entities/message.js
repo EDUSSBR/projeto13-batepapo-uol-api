db4fe95153908a4ffd94ce0a53853670ee44b4cc
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-04-13 10:40:49 -0300

    change 'to' default value to 'Todos'

--- src/entities/message.js
+++ src/entities/message.js
@@ -1,3 +1,3 @@
-export function createMessage(from, text, to="todos", type="status", time=new Date().toLocaleTimeString()){
+export function createMessage(from, text, to="Todos", type="status", time=new Date().toLocaleTimeString()){
     return { from, to, text, type, time}
 }
